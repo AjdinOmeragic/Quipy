099de40dd3ce83c27d85a8738681a1581a5a0be9
Ajdin <ajdin.omeragic.pl@gmail.com>
2025-03-16 17:52:24 +0100

    Initial Commit

---
+++ Frontend/assets/js/custom.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $("main#spapp > section").height($(document).height() - 60);
+
+  var app = $.spapp({ pageNotFound: "error_404" }); // initialize
+
+  // define routes
+  app.route({ view: "about", load: "about.html" });
+  app.route({ view: "view_2", load: "view_2.html" });
+  app.route({ view: "view_3", load: "view_3.html" });
+
+  // run app
+  app.run();
+});
